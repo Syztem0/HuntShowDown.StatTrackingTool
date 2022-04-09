b292afd39797ed8e351c930dabd3d7f7790de9a4
Alexander Nava <AlexanderXN78@Gmail.com>
2022-04-08 19:37:17 -0700

    Boiler Plate Initial Config v1.0.0.0

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HuntShowDown.StatTrackingTool
+{
+    using System.Diagnostics;
+
+    class Program
+    {
+        // TODO:What is the actual effect of having a top level variable public, and are there any security issues with it?
+        public ProgramState{};
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World.");
+            MainLoop();
+        }
+
+        private static void MainLoop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    struct ProgramState
+    {
+        
+    }
+}
+
